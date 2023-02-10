3a9a5fe9ce1147884f701e47db184b2a84bda7fe
Patrick Oakes <poakes@gmail.com>
2023-02-09 20:56:31 -0600

    function to calculate track parameters
    gets x,y coordinates, path length, duration, velocity, etc

--- tcell_migration.py
+++ tcell_migration.py
@@ -32,3 +32,60 @@ def  check_peak_locations(imstack, frame_num = 0, feature_size = 11, minmass = 2
     plt.show()
     
     return
+
+def calculate_track_parameters(cell_tracks_filtered):
+
+    # set particle as the index
+    cell_tracks_filtered_indexed = cell_tracks_filtered.set_index('particle')
+    
+    # make a list of all the unique particle values
+    unique_particle=np.unique(cell_tracks_filtered['particle'].values)
+
+    # empty lists to hold data
+    trajectory_x, trajectory_y = [],[]
+    displacement, pathlength = [],[]
+    path_duration, path_duration_frames = [],[]
+    average_velocity, effective_velocity = [],[]
+    frame_list, first_frame_list = [],[]
+
+    # loop through each particle id
+    for puncta in unique_particle:
+        # make a table of the points for each individual myosin puncta
+        individual_table = cell_tracks_filtered_indexed.loc[puncta]
+        trajectory_x.append(individual_table['x'].values)
+    #     trajectory_xs.append(np.convolve(individual_table['x'], kernel, mode='valid'))
+        trajectory_y.append(individual_table['y'].values)
+    #     trajectory_ys.append(np.convolve(individual_table['y'], kernel, mode='valid'))
+        frame_list.append(individual_table['frame'].values)
+        first_frame_list.append(frame_list[-1][0])
+    #     frame_list_s.append(frame_list[-1][int(np.floor(smoothing_window_size/2)):-int(np.ceil(smoothing_window_size/2))+1])
+        path_duration.append(np.sum(np.diff(frame_list[-1])) * frame_duration)
+        path_duration_frames.append(np.sum(np.diff(frame_list[-1])))
+    #     path_duration_s.append(np.sum(np.diff(frame_list_s[-1]) * frame_duration))
+        pathlength.append(np.sum(np.sqrt((np.diff(trajectory_x[-1])**2 + np.diff(trajectory_y[-1])**2))) * um_per_pixel)
+    #     pathlength_s.append(np.sum(np.sqrt((np.diff(trajectory_xs[-1])**2 + np.diff(trajectory_ys[-1])**2))) * um_per_pixel)
+        average_velocity.append(pathlength[-1] / path_duration[-1])
+        displacement.append(np.sqrt((trajectory_x[-1][-1] - trajectory_x[-1][0])**2 + (trajectory_y[-1][-1] - trajectory_y[-1][0])**2) * um_per_pixel)
+    #     displacement_s.append(np.sqrt((trajectory_xs[-1][-1] - trajectory_xs[-1][0])**2 + (trajectory_ys[-1][-1] - trajectory_ys[-1][0])**2) * um_per_pixel)
+        effective_velocity.append(displacement[-1] / path_duration[-1])
+
+    # make the dataframe
+    cell_trackdata_df = pd.DataFrame()
+    cell_trackdata_df['x'] = trajectory_x
+    cell_trackdata_df['y'] = trajectory_y
+    cell_trackdata_df['frames'] = frame_list
+    cell_trackdata_df['displacement'] = displacement
+    cell_trackdata_df['duration'] = path_duration
+    cell_trackdata_df['pathlength'] = pathlength
+    # cell_trackdata_df['x_smoothed'] = trajectory_xs
+    # cell_trackdata_df['y_smoothed'] = trajectory_ys
+    # cell_trackdata_df['displacement_smoothed'] = displacement_s
+    # cell_trackdata_df['frames_smoothed'] = frame_list_s
+    # cell_trackdata_df['duration_smoothed'] = path_duration_s
+    # cell_trackdata_df['pathlength_smoothed'] = path_length_s
+    cell_trackdata_df['average_velocity'] = average_velocity
+    cell_trackdata_df['effective_velocity'] = effective_velocity
+    cell_trackdata_df['first_frame'] = first_frame_list
+    cell_trackdata_df['duration_frames'] = path_duration_frames
+
+    return cell_trackdata_df
